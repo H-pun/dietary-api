a2bd64327de31ef7e68721d24a892df1eca861ff
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2024-02-20 17:53:54 +0700

    fix: add category and title

--- DataAccess/Models/FoodDiary/DetailFoodDiaryResponse.cs
+++ DataAccess/Models/FoodDiary/DetailFoodDiaryResponse.cs
@@ -9,13 +9,15 @@ namespace Dietary.DataAccess.Models
     {
         public Guid Id { get; set; }
         public Guid IdUser { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public DateTime AddedAt { get; set; }
+        public string FilePath { get; set; }
         public float TotalCaloriesToday { get; set; }
         public float MaxDailyBmrCalories { get; set; }
         public float TotalFoodCalories { get; set; }
         public string Status { get; set; }
         public string Feedback { get; set; }
         public dynamic Foods { get; set; }
-        public DateTime AddedAt { get; set; }
-        public string FilePath { get; set; }
     }
 }
